4b10c4ba49d939f3860bafe08d34e2b58c821090
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-02-21 12:13:50 +0000

    hijkjnjkn

---
+++ qwerty/models/5/model_5_SQLStatement_1.sql
@@ -0,0 +1,18 @@
+{{
+  config({    
+    "materialized": "table",
+    "alias": "prophecy__temp_5_post_SQLStatement_1_0",
+    "database": "database",
+    "schema": "default"
+  })
+}}
+
+WITH SQLStatement_1 AS (
+
+  
+
+)
+
+SELECT *
+
+FROM SQLStatement_1
