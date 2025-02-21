69402675bab5bf8ec9b910e436b4f2ae9ef6e939
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-02-21 08:47:05 +0000

    Reverted to 'tryrtyrtyry'

--- qwerty/models/5.sql
+++ qwerty/models/5.sql
@@ -4,16 +4,8 @@ WITH model AS (
   
   FROM {{ source('hive_metastore.arun123', 'model') }}
 
-),
-
-Reformat_1 AS (
-
-  SELECT * 
-  
-  FROM model AS in0
-
 )
 
 SELECT *
 
-FROM Reformat_1
+FROM model
